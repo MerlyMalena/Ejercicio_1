10daec8802de88d7890434bffcf47f6f44996ec8
MerlyMalena <merlymalena2303@hotmail.com>
2026-01-19 19:03:59 -0400

    Guarda datos del triangulo

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@
 
 using System;
 using System.Drawing;
+using System.Collections;
+using System.Runtime.CompilerServices;
 
 class Program
 {
@@ -19,7 +21,8 @@ class Program
     
     public class Figura
     {
-        public String color;
+        public String color {get;set;}
+        
         public int lados;
         public virtual void cambiarColor()
         {
@@ -37,10 +40,7 @@ class Program
             lados=4;
             longitud=0;
         }
-        
-        
-       
-       
+
        
     }
 
@@ -58,18 +58,21 @@ class Program
         private static Triangulo? instancia;
 
         int lados {get;set;}
-        int longitudLado1 {get;set;}
 
-        int longitudLado2 {get;set;}
+        int color {get;set;}
+        public int longitudLado1 {get;set;}
 
-        int longitudLado3 {get;set;}
+        public int longitudLado2 {get;set;}
+
+        public int longitudLado3 {get;set;}
 
 
-        public Triangulo(){
+        private Triangulo(){
         lados=3;
         longitudLado1=0;
         longitudLado2=0;
         longitudLado3=0;
+  
         }
 
         public  static Triangulo ObtenerInstancia(){
@@ -86,15 +89,21 @@ class Program
     }
     static void Main(string[] args)
     {
-        Console.WriteLine("Menú");
-        Console.WriteLine("1. Crear una nueva figura");
-        Console.WriteLine("2. Ver las figuras creadas");
-        Console.WriteLine("");
-        Console.Write("\t --> ");
-        int opcion= int.Parse(Console.ReadLine());
+        ArrayList figuras = new ArrayList();
+        int opcion=0;
+
 
         do
         {
+
+              Console.WriteLine("Menú");
+        Console.WriteLine("1. Crear una nueva figura");
+        Console.WriteLine("2. Ver las figuras creadas");
+        Console.WriteLine("3. Salir");
+        Console.WriteLine("");
+        Console.Write("\t --> ");
+        opcion= int.Parse(Console.ReadLine());
+
             if (opcion == 1)
             {
                 Console.Clear();
@@ -122,11 +131,74 @@ class Program
                     Console.WriteLine("El color de su cuadrado es: "+ c1.color);
                     Console.WriteLine("La longitud de su cuadrado es: "+ c1.longitud);
 
+                    figuras.Add(c1);
+
+                    foreach (object item in figuras)
+                    {
+                        if (item is Cuadrado cuad)
+                        Console.WriteLine("Cuadrado. Color:"+cuad.color+ "\nLongitud:"+cuad.longitud);
+                    }
+                    Console.WriteLine("Presione cualquier tecla para continuar");
+                    Console.ReadKey();
+                }
+
+                else if (opcionFigura == 2)
+                {
+                    Console.WriteLine("\t -- Crear un rectangulo --");
+                }
+
+                 else if (opcionFigura == 3)
+                {
+                    Console.WriteLine("\t -- Crear un triangulo --");
+                    Console.WriteLine("\t ¿De qué color desea que sea su triángulo?");
+                    var t1 = Triangulo.ObtenerInstancia();
+                    t1.color=Console.ReadLine();
+
+                    Console.WriteLine("\t ¿De qué tamaño desea el primer lado de su triángulo?");
+                    t1.longitudLado1=int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\t ¿De qué tamaño desea el segundo lado de su triángulo?");
+                    t1.longitudLado2=int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\t ¿De qué tamaño desea el tercer lado de su triángulo?");
+                    t1.longitudLado3=int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("El triangulo tiene los siguientes lados: "+t1.longitudLado1+", "+t1.longitudLado2+", "+t1.longitudLado3);
+                    Console.WriteLine("El color de su triangulo es: "+ t1.color);
+
+                     figuras.Add(t1);
+
+                    foreach (object item in figuras)
+                    {
+                        if (item is Triangulo tr)
+                        Console.WriteLine("Triangulo. Color:"+tr.color+ "\nLongitud:"+tr.longitudLado1+", "+t1.longitudLado2+", "+t1.longitudLado3);
+                    }
                     Console.WriteLine("Presione cualquier tecla para continuar");
                     Console.ReadKey();
                 }
 
 
+            }
+
+            else if (opcion == 2)
+            {
+                  Console.WriteLine("\t -- Lista de figuras creadas --");
+                  Console.WriteLine("\t -- Triangulos--");
+
+                foreach (object item in figuras)
+                {
+                    if (item is Triangulo tr)
+                    {
+                        Console.WriteLine("\tTriangulo. Color:" + tr.color + "\nLongitud:" + tr.longitudLado1 + ", " + tr.longitudLado2 + ", " + tr.longitudLado3);
+                    }
+                    else
+                    {
+                    Console.WriteLine("No se han creado triángulos");
+                    }
+                }
+
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.ReadKey();
             }
         }
         while(opcion!=3);
